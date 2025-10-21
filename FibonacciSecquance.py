ae1642729e9a4587f04dd916d8e03415055a74e1
Baygeldi <baygeldi0718@gmail.com>
2025-10-22 00:21:27 +0800

    InsertVersionAdded

--- FibonacciSecquance.py
+++ FibonacciSecquance.py
@@ -1,11 +1,9 @@
 
-a=0
-b=1
+n = int(input("Enter how many terms you want: "))
 
-while b < 1000:
-    print(b)
-    a, b = b , a + b
+a = 0
+b = 1
 
-
-
-
+for i in range(n):
+    print (a, end=" ")
+    a , b = b, a + b
